51d5b5cb851b589b50a2a0d66d4c347a453b654d
Yunjeong Gu <yunjasgu@gmail.com>
2024-10-20 09:39:53 +0800

    ErrorController class

--- Framework/Router.php
+++ Framework/Router.php
@@ -2,6 +2,8 @@
 
 namespace Framework;
 
+use App\Controllers\ErrorController;
+
 class Router {
     protected $routes = [];
 
@@ -102,6 +104,6 @@ class Router {
             }
         }
 
-        $this->error();
+        ErrorController::notFound();
     }
 }
